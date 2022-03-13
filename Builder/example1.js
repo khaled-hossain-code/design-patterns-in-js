92453a8be1631bfa732c996947d2f26d2050bd7d
Md. Kaled Hossain <md.khaled.eee@gmail.com>
2022-03-13 20:11:52 +0600

    Add example 1

--- Builder/example1.js
+++ Builder/example1.js
@@ -1,4 +1,22 @@
 // we need to create a program which returns a piece of html
 
-// solution1
-const hello = "";
+// lets return paragraph
+const hello = "hello";
+const html = [];
+html.push("<p>");
+html.push(hello);
+html.push("</p>");
+console.log(html.join(""));
+
+//lets return list of words
+const words = ["hello", "world"];
+const html1 = [];
+html1.push("<ul>\n");
+
+words.forEach((word) => {
+  html1.push(`  <li>${word}</li>\n`);
+});
+
+html1.push("</ul>");
+
+console.log(html1.join(""));
